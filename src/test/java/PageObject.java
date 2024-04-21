fb78f7f04875b45e80323b374d66785fd8f4a777
subramanyaganesh <subramanya1174@gmail.com>
2024-04-21 15:58:50 -0500

    added commnts part 1

--- src/test/java/PageObject.java
+++ src/test/java/PageObject.java
@@ -5,6 +5,7 @@ public class PageObject {
 
     protected WebDriver driver;
 
+    //page object model to ensure that the several pages can be automated using the same template
     public PageObject(WebDriver driver) {
         this.driver = driver;
         PageFactory.initElements(driver,this);
--- src/test/java/Utils.java
+++ src/test/java/Utils.java
@@ -3,6 +3,7 @@ import java.util.List;
 
 public class Utils {
 
+    // all static values are placed in this file
     static List<String> group1 = new ArrayList<>(List.of("0", "1", "2"));//List.of is immutable
     static List<String> group2 = new ArrayList<>(List.of("3", "4", "5"));
     static List<String> group3 = new ArrayList<>(List.of("6", "7", "8"));
@@ -11,7 +12,6 @@ public class Utils {
 
     static String answer = "";
     final static String BASE_URL = "http://sdetchallenge.fetch.com/";
-    //final static String BASE_URL = "https://formy-project.herokuapp.com/form/";
     final static String CHROME_DRIVER_LOCATION = "chromedriver";
 
 }
